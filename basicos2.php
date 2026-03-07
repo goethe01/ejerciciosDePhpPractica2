6b3679bf671ed4f343ed318561e208e8813e2851
goethe01 <124703630+goethe01@users.noreply.github.com>
2026-03-07 09:10:43 -0500

    Ejercicios del 1 al 3 solucionados.

---
+++ basicos2.php
@@ -0,0 +1,15 @@
+<?php
+//punto 1
+$numeros = [4,7,2,9,5];
+echo "La suma es: " . array_sum($numeros);
+echo "<hr>";
+
+//punto 2
+echo "El número mayor es: " . max($numeros);
+echo "<hr>";
+
+//punto 3
+echo "El número menor es: " . min($numeros);
+echo "<hr>";
+
+//punto 4
