ddaecf9e8fde10f63aa2dfce9220d25b0183474a
goethe01 <124703630+goethe01@users.noreply.github.com>
2026-03-07 09:57:08 -0500

    Solucion de los punto del 4-6

--- basicos2.php
+++ basicos2.php
@@ -13,3 +13,28 @@ echo "El número menor es: " . min($numeros);
 echo "<hr>";
 
 //punto 4
+$frutas = ["manzana", "pera", "manzana", "banana", "pera", "manzana"];
+
+foreach(array_count_values($frutas) as $key => $values){
+    echo $key . " => " . $values . "<br>";
+}
+echo "<hr>";
+// punto 5
+$numeros = [5, 12, 8, 20, 3, 15];
+
+$filtrados = array_filter( $numeros, fn($num) => $num > 10);
+foreach($filtrados as $key){
+    echo $key ."<br>";
+}
+echo "<hr>";
+// punto 6 
+
+$numeros = [2, 3, 4, 5];
+$cuadrados = array_map(fn($num) => $num**2, $numeros);
+
+foreach($cuadrados as $key){
+    echo $key . "<br>";
+}
+echo "<hr>";
+
+// punto 7
