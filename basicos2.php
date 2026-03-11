8c24b52139d17f6d55c1c940a59fb7735bde05db
goethe01 <124703630+goethe01@users.noreply.github.com>
2026-03-11 11:50:09 -0500

    Terminado los ejercicios incluyendo el bonus

--- basicos2.php
+++ basicos2.php
@@ -49,7 +49,108 @@ if (in_array($buscar, $colores)){
 // punto 8
 echo "<hr>";
 $numeros = [9, 3, 7, 1, 6];
-sort($numeros,); 
+sort($numeros); 
 foreach($numeros as $key){
-    echo $key;
+    echo $key . ", ";
 }
+
+// punto 9
+echo "<hr>";
+$numeros = [2, 10, 5, 1];
+rsort($numeros);
+
+foreach($numeros as $key){
+    echo $key . ", ";
+}
+
+// punto 10 
+
+echo "<hr>";
+
+$palabras = ["PHP", "es", "genial"];
+
+$palabras = implode(" " , $palabras);
+
+var_dump($palabras);
+
+
+// punto 11
+
+echo "<hr>";
+
+$texto = "rojo,verde,azul";
+
+$texto = explode(",", $texto);
+
+foreach($texto as $key){
+    echo $key . " ";
+}
+
+// punto 12
+echo "<hr>";
+
+$numeros = [1,2,3,4,5,6];
+$sum_pares = 0;
+$pares= [];
+foreach($numeros as $key){
+    if ($key%2 == 0){
+        $sum_pares += $key;
+        $pares[] = $key;  
+    }
+}
+
+echo $sum_pares;
+echo "<br>";
+foreach($pares as $key){
+    echo "$key ";
+}
+
+//punto 13
+echo "<hr>";
+$palabras = ["sol", "computadora", "mesa", "programacion", "php"];
+
+$contador = 0;
+foreach($palabras as $key){
+    if (strlen($key) > 5){
+        $contador++;
+    }
+}
+echo "Palabras largas: " . $contador;
+
+// punto 14 
+
+echo "<hr>";
+$datos = [10, "hola", true, "php", 3.14, "codigo"];
+$datos_limpios = array_filter($datos, fn($valor) =>  "string" == gettype($valor));
+
+foreach($datos_limpios as $key){
+    echo $key . " ";
+}
+
+// punto 15
+echo "<hr>";
+
+$notas = [4, 3, 5, 2, 4];
+
+$suma = array_reduce($notas, fn($acumulado, $valor)=> $acumulado + $valor, 0);
+
+echo "Promedio: " . $suma/ count($notas);
+
+//Ejercicio Bonus
+echo "<hr>";
+
+$ventas = [120, 340, 90, 560, 230];
+
+$suma = array_reduce($ventas, fn($contador, $valor)=> $contador + $valor, 0 );
+echo "Total: " . $suma . "<br>";
+
+echo "Promedio: " . $suma / count($ventas) . "<br>" ;
+
+rsort($ventas);
+
+echo "Mayor venta: " . $ventas[0] . "<br>";
+
+sort($ventas);
+
+echo "Menor venta: " . $ventas[0];
+
